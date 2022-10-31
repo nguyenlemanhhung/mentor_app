45425e9e891ec0eb002b837b6f5d2595a39d4544
nguyenlemanhhung <github.nohssiw1905@gmail.com>
2022-10-31 10:02:27 +0700

    test create firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,25 @@ import 'package:flutter/material.dart';
 import 'package:mentor_app/screen/auth_screen/sign_in/sign_in_screen.dart';
 import 'package:mentor_app/screen/splash_screen/splash_screen.dart';
 import 'package:mentor_app/theme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'routes.dart';
 
 import 'screen/main_screen/main_screen.dart';
 
-void main() {
-  runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => MyApp(), // Wrap your app
-    ),
-  );
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
+// void main() {
+//   runApp(
+//     DevicePreview(
+//       enabled: !kReleaseMode,
+//       builder: (context) => MyApp(), // Wrap your app
+//     ),
+//   );
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
